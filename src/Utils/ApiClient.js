be349acf61f8d78d0b2e84cc44cece2422d766ca
Kiran TJ <kiran.thomas.jacob@gmail.com>
2025-01-13 13:10:20 +0530

    origin url changed

--- src/Utils/ApiClient.js
+++ src/Utils/ApiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api_client = axios.create({
-    // baseURL: process.env.API_URL || 'http://localhost:8080',
+    // baseURL: 'http://localhost:8080',
     baseURL: 'https://blogcove-backend.onrender.com',
     withCredentials: true // Include cookies with requests
 });
